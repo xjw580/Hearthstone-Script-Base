9af5ba532566a92f11f0508b9f22a43bc49daeee
zergqueen <xjw580@qq.com>
2025-07-20 19:04:33 +0800

    refactor: 🧬️增加构建信息读取

---
+++ src/main/kotlin/club/xiaojiawei/hsscriptbase/const/BuildInfo.kt
@@ -0,0 +1,35 @@
+package club.xiaojiawei.hsscriptbase.const
+
+import club.xiaojiawei.hsscriptbase.config.log
+import java.util.Properties
+
+/**
+ * @author 肖嘉威 xjw580@qq.com
+ * @date 2024/9/24 9:53
+ */
+object BuildInfo {
+
+    val VERSION: String
+
+    val ARTIFACT_ID: String
+
+    init {
+        val properties = Properties()
+        try {
+            BuildInfo::class.java.getClassLoader().getResourceAsStream("build.info").use { resourceStream ->
+                if (resourceStream == null) {
+                    log.error { "build.info file is not found in the classpath." }
+                } else {
+                    properties.load(resourceStream)
+
+                }
+            }
+        } catch (e: Exception) {
+            log.warn(e) { "无法读取版本号" }
+        }
+        VERSION = properties.getProperty("version", "UNKNOWN")
+        ARTIFACT_ID = properties.getProperty("artifactId", "UNKNOWN")
+    }
+
+
+}
